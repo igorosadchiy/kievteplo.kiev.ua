30fb9345c4f1cadd09883b6e90a69931ec07caef
Igor Osadchiy <egor.osadchiy@gmail.com>
2018-12-03 21:11:36 +0200

    betta 1 ready

---
+++ js/main.js
@@ -0,0 +1,87 @@
+(function ($) {
+	"use strict";
+	
+	/*----------------------------
+    Smooth scroll animation
+    ------------------------------ */
+	$('.mainmenu li a, .logo a,.slicknav_nav li a').on('click', function () {
+		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') &&
+            location.hostname === this.hostname) {
+		  var $target = $(this.hash);
+		  $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+		  if ($target.length) {
+			var targetOffset = $target.offset().top;
+			$('html,body')
+			.animate({scrollTop: targetOffset}, 2000);
+		   return false;
+		  }
+		}
+	});
+	
+	/*----------------------------
+    Scroll to Top
+    ------------------------------ */
+	$(window).on('scroll', function() {
+		if ($(this).scrollTop() > 600) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	$('.scrollToTop').on('click', function () {
+		$('html, body').animate({scrollTop : 0},2000);
+		return false;
+	});
+	    
+    /*----------------------------
+    Modal submit
+    ------------------------------ */
+    $('#check').on('click', function() {
+        if ($("#check").prop("checked")) {
+            $('#button').attr('disabled', false);
+        } else {
+            $('#button').attr('disabled', true);
+        }
+    });
+
+    $('#contactForm').on('submit', function(event) {
+        
+        event.preventDefault();
+
+        var form = $('#contactForm'),
+            button = $('#button'),
+            answer = $('#answer'),
+            loader = $('#loader');
+
+        $.ajax({
+            url: 'handler.php',
+            type: 'POST',
+            data: form.serialize(),
+            beforeSend: function() {
+                answer.empty();
+                button.attr('disabled', true).css('margin-bottom', '20px');
+                loader.fadeIn();
+            },
+            success: function(result) {
+                loader.fadeOut(300, function() {
+                    answer.text(result);
+                });
+                form.find('.form-control').val(' ');
+                button.attr('disabled', false);
+            },
+            error: function() {
+                loader.fadeOut(300, function() {
+                    answer.text('Произошла ошибка! Попробуйте позже.');
+                });
+                button.attr('disabled', false);
+            }
+        });
+
+    });
+    
+    /*----------------------------
+    Init WOW amimate
+    ------------------------------ */
+    new WOW().init();
+    
+}(jQuery));
